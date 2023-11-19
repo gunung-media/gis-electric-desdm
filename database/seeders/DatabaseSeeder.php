f63874472624c09cb844e2932e771b8deb523311
Richie-Z <zakariarichie@yahoo.com>
2023-11-19 14:30:20 +0700

    feat(db): introduce indonesia

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use KodePandai\Indonesia\IndonesiaDatabaseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,5 +18,7 @@ class DatabaseSeeder extends Seeder
             'username' => 'admin',
             'password' => bcrypt('admin')
         ]);
+
+        $this->call(IndonesiaDatabaseSeeder::class);
     }
 }
