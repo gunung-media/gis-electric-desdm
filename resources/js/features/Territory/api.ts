d1082f490bf6c4ffd1302c33d2f8250a6de4f55e
Richie-Z <zakariarichie@yahoo.com>
2023-11-19 22:03:02 +0700

    fix(territory): the param type

--- resources/js/features/Territory/api.ts
+++ resources/js/features/Territory/api.ts
@@ -6,4 +6,4 @@ const PROVINCE_NAME = "Kalimantan Tengah"
 
 export const getCities = async (): Promise<AxiosResponse<{ data: CityType[] }>> => await axios.get(`${URL}/cities`, { params: { 'province_name': PROVINCE_NAME } })
 
-export const getDistricts = async (cityCode: number): Promise<AxiosResponse<{ data: DistrictType[] }>> => await axios.get(`${URL}/districts`, { params: { 'city_code': cityCode } })
+export const getDistricts = async (cityCode: number | string): Promise<AxiosResponse<{ data: DistrictType[] }>> => await axios.get(`${URL}/districts`, { params: { 'city_code': cityCode } })
