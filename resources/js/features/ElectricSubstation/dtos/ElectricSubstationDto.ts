82043ff6b2807055e2ba08bff2d253e4685341e7
Richie-Z <zakariarichie@yahoo.com>
2023-11-19 21:50:18 +0700

    feat(electric-substation): introduce the dto

---
+++ resources/js/features/ElectricSubstation/dtos/ElectricSubstationDto.ts
@@ -0,0 +1,8 @@
+export interface ElectricSubstationDTO {
+    name: string
+    district_id: string | number
+    city_id?: string | number
+    description?: string
+    latitude: string | number
+    longitude: string | number
+}
