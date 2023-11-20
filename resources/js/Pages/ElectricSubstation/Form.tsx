37b68510d78782b7a0c2646b7dff9c5a1aa7cb57
Richie-Z <zakariarichie@yahoo.com>
2023-11-20 13:39:07 +0700

    enhance(electric-substation): add functionality to form

--- resources/js/Pages/ElectricSubstation/Form.tsx
+++ resources/js/Pages/ElectricSubstation/Form.tsx
@@ -1,20 +1,22 @@
 import { AuthenticatedLayout } from "@/layouts/AuthenticatedLayout"
-import { useEffect, useState } from "react"
+import { FormEventHandler, useEffect, useState } from "react"
 import L from "leaflet"
-import { Input, OptionType, Select } from "@/common/components"
+import { Input, InputError, OptionType, Select } from "@/common/components"
 import latLangKalteng from "@/common/constants/latLangKalteng"
 import { DistrictType, getCities, getDistricts, CityType } from "@/features/Territory"
-import { useForm } from "@inertiajs/react"
+import { useForm, usePage } from "@inertiajs/react"
 import { ElectricSubstationDTO } from "@/features/ElectricSubstation"
+import Swal from "sweetalert2"
+import { PageProps } from "@/types"
 
 export default function Form() {
+    const { errors, session } = usePage<PageProps>().props
     const [cities, setCities] = useState<CityType[]>([])
     const [districts, setDistricts] = useState<DistrictType[]>([])
     const [selectedDistrict, setSelectedDistrict] = useState<OptionType<DistrictType> | null>(null)
     const [map, setMap] = useState<L.Map>()
     const [marker, setMarker] = useState<L.Marker>()
-    const [latitude, setLatitude] = useState<number>(latLangKalteng[0])
-    const { data: dto, setData } = useForm<ElectricSubstationDTO>()
+    const { data: dto, setData, post } = useForm<ElectricSubstationDTO>()
 
     useEffect(() => {
         (async () => {
@@ -32,8 +34,7 @@ export default function Form() {
 
         marker.on('dragend', function() {
             const position = marker.getLatLng();
-            setLatitude(position.lat)
-            setData('longitude', position.lng)
+            setData(data => ({ ...data, latitude: position.lat, longitude: position.lng }))
         });
 
         setMap(mapRef)
@@ -47,21 +48,42 @@ export default function Form() {
     const handleCityChange = async (e: OptionType<CityType>) => {
         setData("city_id", e.value.code)
         setSelectedDistrict(null)
-        setData("district_id", null)
+        setData("district_code", null)
         const { data: { data } } = await getDistricts(e.value.code)
         setDistricts(data)
     }
 
     const handleDistrictChange = async (e: OptionType<DistrictType>) => {
-        setData("district_id", e.value.code)
+        setData("district_code", e.value.code)
         setSelectedDistrict(e)
         const { data: { data } } = await getDistricts(e.value.city_code)
         setDistricts(data)
-        const latLang = [parseInt(e.value.latitude), parseInt(e.value.longitude)] as L.LatLngExpression
-        map!.panTo(latLang)
-        marker!.setLatLng(latLang)
-        setLatitude(parseInt(e.value.latitude))
-        setData('longitude', e.value.longitude)
+        const latLang = [parseInt(e.value.latitude), parseInt(e.value.longitude)]
+        map!.panTo(latLang as L.LatLngExpression)
+        marker!.setLatLng(latLang as L.LatLngExpression)
+        setData(data => ({ ...data, latitude: latLang[0], longitude: latLang[1] }))
+    }
+
+    const handleSubmit: FormEventHandler = (e) => {
+        e.preventDefault()
+        post(route('admin.gardu_listrik.store'), {
+            onError: (e) => {
+                if (e.error) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: e.error,
+                    });
+                }
+            },
+            onSuccess: (e) => {
+                Swal.fire({
+                    icon: "success",
+                    title: "Sukses",
+                    text: "Sukses Menambah",
+                });
+            }
+        })
     }
 
     return (
@@ -79,13 +101,18 @@ export default function Form() {
                     <div className="card">
                         <div className="card-body">
                             <h4 className="card-title">Additional Data</h4>
-                            <form className="forms-sample">
+                            <InputError message={errors.error} />
+                            <form className="forms-sample" onSubmit={handleSubmit}>
                                 <Select title="Kabupaten/Kota" data={cities} onChange={(e) => handleCityChange(e as OptionType<CityType>)} />
                                 <Select title="Kecamatan" data={districts} onChange={(e) => handleDistrictChange(e as OptionType<DistrictType>)} value={selectedDistrict} />
+                                <InputError message={errors.district_code} />
                                 <Input title="Nama Gardu Listrik" onChange={(e) => setData("name", e.target.value)} />
+                                <InputError message={errors.name} />
                                 <Input title="Deskripsi" onChange={(e) => setData("description", e.target.value)} />
-                                <Input title="latitude" readOnly={true} value={latitude} />
-                                <Input title="longitude" readOnly={true} value={dto.longitude ?? latLangKalteng[1]} />
+                                <Input title="latitude" readOnly={true} value={dto.latitude} />
+                                <InputError message={errors.latitude} />
+                                <Input title="longitude" readOnly={true} value={dto.longitude} />
+                                <InputError message={errors.longitude} />
                                 <button type="submit" className="btn btn-primary me-2">Submit</button>
                                 <button className="btn btn-light">Cancel</button>
                             </form>
@@ -93,6 +120,6 @@ export default function Form() {
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        </AuthenticatedLayout >
     )
 }
