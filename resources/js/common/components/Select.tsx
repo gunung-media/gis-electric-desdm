9a23b2b0bddc62ca04c6ed9534069f8389778b0a
Richie-Z <zakariarichie@yahoo.com>
2023-11-25 17:35:01 +0700

    enhance(select): add default value

--- resources/js/common/components/Select.tsx
+++ resources/js/common/components/Select.tsx
@@ -1,4 +1,5 @@
 import { TerritoryType } from "@/features/Territory"
+import { useEffect, useState } from "react"
 import ReactSelect, { Props } from "react-select"
 
 export type OptionType<T> = {
@@ -6,12 +7,21 @@ export type OptionType<T> = {
     label: string
 }
 
-export const Select = ({ title, data, ...props }: Props & { title: string, data: TerritoryType[] }) => {
+export const Select = ({ title, data, selectedId, ...props }: Props & { title: string, data: TerritoryType[], selectedId?: number, }) => {
     const option: OptionType[] = data.map((val) => ({ value: val, label: val.name }))
+    const [defaultValue, setDefaultValue] = useState<OptionType>()
+    useEffect(() => {
+        const label = data.filter(val => val.code == selectedId)[0]?.name;
+        setDefaultValue({
+            value: selectedId,
+            label: label
+        })
+        console.log(label)
+    }, [data, selectedId])
     return (
         <div className="form-group">
             <label htmlFor={title}>{title}</label>
-            <ReactSelect {...props} id={title} options={option} />
+            <ReactSelect {...props} id={title} options={option} value={defaultValue} />
         </div>
     )
 }
