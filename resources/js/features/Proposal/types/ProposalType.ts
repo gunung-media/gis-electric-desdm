6fcfe0af427c6ceb5337b8f146e4a40ff3d35543
Richie-Z <zakariarichie@yahoo.com>
2023-11-26 18:05:03 +0700

    feat(proposal): introduce proposal feature

---
+++ resources/js/features/Proposal/types/ProposalType.ts
@@ -0,0 +1,23 @@
+import { CommonTableInterface } from "@/common/interface/CommonTableInterface"
+import { VillageType } from "@/features/Territory"
+import { ProposalTracking } from "./ProposalTrackingType"
+
+export interface ProposalType extends CommonTableInterface {
+    full_name: string
+    identity_number: string
+    email: string
+    phone_number: string
+    village_code: string
+    address: string
+    latitude: string | null
+    longitude: string | null
+    proposal_type: string
+    description: string | null
+    document_path: string
+    estimated_cost: number | null
+    priority: 'Tinggi' | 'Sedang' | 'Rendah'
+    additional_note: string | null
+    latest_status: string
+    village: VillageType
+    trackings: ProposalTracking[]
+}
---
+++ resources/js/features/Proposal/types/index.ts
@@ -0,0 +1,2 @@
+export * from "./ProposalType"
+export * from "./ProposalTrackingType"
