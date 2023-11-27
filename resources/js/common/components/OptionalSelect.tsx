06c3d4179d22ff082bd1555954f70da26645504f
Richie-Z <zakariarichie@yahoo.com>
2023-11-27 16:45:25 +0700

    enhance(component): optional select rm the push Lainnya

--- resources/js/common/components/OptionalSelect.tsx
+++ resources/js/common/components/OptionalSelect.tsx
@@ -8,7 +8,6 @@ export const OptionalSelect: FC<{
     onSelected: (val: string) => void
 } & Props> = ({ title, defaultChoice, onSelected, ...props }) => {
     const option: OptionType<string>[] = defaultChoice.map((val) => ({ value: val, label: val }))
-    option.push({ value: "Lainnya", label: "Lainnya" })
     const [selected, setSelected] = useState<OptionType<string>>()
 
     const handleSelectChange = (e: OptionType<string>) => {
