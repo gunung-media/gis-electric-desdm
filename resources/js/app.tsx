10b9c129da90e65180c3daca1e309748bae68ac6
Richie-Z <zakariarichie@yahoo.com>
2023-11-27 16:34:48 +0700

    feat(package): introduce react-bootstrap

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,6 @@
 import './bootstrap';
 import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
