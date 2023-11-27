579e3a9267b7bbe3e65f62bb267b26404687b7bd
Richie-Z <zakariarichie@yahoo.com>
2023-11-27 16:35:32 +0700

    feat(proposa): introduce dto

---
+++ resources/js/features/Proposal/dtos/ProposalDto.ts
@@ -0,0 +1,19 @@
+import { PriorityEnum } from "@/common/enums"
+
+export interface ProposalDTO {
+    full_name: string
+    identity_number: string
+    email: string
+    phone_number: string
+    village_code: string
+    address: string
+    latitude?: string | null
+    longitude?: string | null
+    proposal_type: string
+    description?: string | null
+    document_path: File
+    estimated_cost?: number | null
+    priority: PriorityEnum
+    additional_note?: string | null
+}
+
