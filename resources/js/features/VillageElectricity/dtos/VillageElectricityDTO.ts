62ad52ace4b8509658fba0d67123a9ccf14d1b8b
Richie-Z <zakariarichie@yahoo.com>
2023-11-29 01:42:37 +0700

    feat(village-electricity): introduce the features

---
+++ resources/js/features/VillageElectricity/dtos/VillageElectricityDTO.ts
@@ -0,0 +1,7 @@
+export interface VillageElectricityDTO {
+    households_with_electricity: number
+    households_without_electricity: number
+    network_length: number
+    village_potential: string
+    village_code: string | number
+}
---
+++ resources/js/features/VillageElectricity/types/VillageElectricityType.ts
@@ -0,0 +1,11 @@
+import { CommonTableInterface } from "@/common/interface";
+import { VillageType } from "@/features/Territory";
+
+export interface VillageElectricityType extends CommonTableInterface {
+    village: VillageType
+    households_with_electricity: number
+    households_without_electricity: number
+    network_length: number
+    village_potential: string
+    electricity: boolean
+}
