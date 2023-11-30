afbbdff56dbbf0c4860a7f8f23078a6a3551e928
Richie-Z <zakariarichie@yahoo.com>
2023-11-30 16:53:54 +0700

    enhance(layout): footer text

--- resources/js/Layouts/partials/Footer.tsx
+++ resources/js/Layouts/partials/Footer.tsx
@@ -4,7 +4,8 @@ export const Footer = () => {
     return (
         <footer className="footer">
             <div className="d-sm-flex justify-content-center justify-content-sm-between">
-                <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © {year}. <a href="gunungmedia.com" target="_blank">Gunung Media</a>.  All rights reserved.</span>
+                <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © {year}.Dinas Energi dan Sumber Daya Mineral Provinsi Kalimantan Tengah.  All rights reserved.</span>
+                <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Designed by <a href="https://gunungmedia.com" target="_blank">Gunung Media</a></span>
             </div>
         </footer>
     )
